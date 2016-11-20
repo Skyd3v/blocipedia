072b00e9b7d36174c55fa2c5d8d6c980ec86e26f
Alex Anthopoulos <a.anthopoulos02@gmail.com>
2016-11-20 13:27:21 +0000

    adding bootstrap and small changes

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,7 @@
 Rails.application.routes.draw do
   devise_for :users
-  get 'welcome/index'
-
-  get 'welcome/about'
+  #Assign about page to be correctly linked
+  get 'about' => 'welcome#about'
   #Assign welcome page as index
   root 'welcome#index'
 

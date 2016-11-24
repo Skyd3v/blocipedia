4df124ca74022924ef27355be967a245bc7ca9b5
Alex Anthopoulos <a.anthopoulos02@gmail.com>
2016-11-24 21:20:30 +0000

    adding wiki controller and functionalities

---
+++ app/controllers/wikis_controller.rb
@@ -0,0 +1,34 @@
+class WikisController < ApplicationController
+    def index
+        @wikis = Wiki.all
+    end
+
+    def show
+       @wiki = Wiki.find(params[:id])
+    end
+
+    def new
+        @wiki = Wiki.new
+    end
+
+    def create
+        # #9
+        @wiki = Wiki.new
+        @wiki.title = params[:wiki][:title]
+        @wiki.body = params[:wiki][:body]
+
+        # #10
+        if @wiki.save
+            # #11
+            flash[:notice] = 'Wiki was saved.'
+            redirect_to @wiki
+        else
+            # #12
+            flash.now[:alert] = 'There was an error saving the wiki. Please try again.'
+            render :new
+        end
+    end
+
+    def edit
+    end
+end

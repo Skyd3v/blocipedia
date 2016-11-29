e0d40e333b9c92210d2f4e4c1d68771ae058cc23
Alex Anthopoulos <a.anthopoulos02@gmail.com>
2016-11-29 20:26:57 +0000

    add policies

--- app/policies/wiki_policy.rb
+++ app/policies/wiki_policy.rb
@@ -1,3 +1,32 @@
 class WikiPolicy < ApplicationPolicy
+    attr_reader :user, :wiki
 
+    def initialize(user, wiki)
+        @user = user
+        @wiki = wiki
+    end
+
+    def update?
+        user.present? || user.admin?
+    end
+
+    def new?
+        user.present? || user.admin?
+    end
+
+    def create?
+        user.present? || user.admin?
+    end
+
+    def show?
+        user.present? || !user.present?
+    end
+
+    def edit?
+        user.present? || user.admin?
+    end
+
+    def destroy?
+        user.admin?
+    end
 end

faa4f489a3a4c135aa2247d5c2e44a4e5e0de195
Alex Anthopoulos <a.anthopoulos02@gmail.com>
2016-12-05 22:50:12 +0000

    collaboration working finally

--- app/models/wiki.rb
+++ app/models/wiki.rb
@@ -1,6 +1,6 @@
 class Wiki < ActiveRecord::Base
   belongs_to :user
   has_many :collaborations
-  has_many :users, through: :collaborations
+  has_many :collaborators, through: :collaborations, source: :user
   scope :visible_to, -> (user) { user.role == 'premium' || user.role == 'admin' ? all : where(private: false) }
 end
